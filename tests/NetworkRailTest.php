74fc7ec3eedc0bc9f83fe45baa702031ac4a89a9
Conor Haining <conor.haining@gmail.com>
2016-12-06 22:08:40 +0000

    Initial tests

---
+++ tests/NetworkRailTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use conorhaining/php-openraildata/NetworkRail;
+
+class NetworkRailTest extends PHPUnit_Framework_TestCase{
+
+	public function testConstructor(){
+		$username = "JohnDoe";
+		$password = "Password123";
+		$securityToken = "7f8cac09-f446-e51f-a324-c528843e36f5";
+
+		$NR = new NetworkRail();
+
+		$this->assertEquals($username, "JohnDoe");
+		$this->assertEquals($password, "Password123");
+		$this->assertEquals($securityToken, "7f8cac09-f446-e51f-a324-c528843e36f5");
+	}
+}
