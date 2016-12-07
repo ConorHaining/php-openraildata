330080fc7f5d8d377f9c22007ec68e1527bf2a6b
Conor Haining <conor.haining@gmail.com>
2016-12-07 21:48:12 +0000

    fixed testing

--- tests/NetworkRailTest.php
+++ tests/NetworkRailTest.php
@@ -1,18 +1,16 @@
 <?php
 
-use conorhaining\phpopenraildata\NetworkRail;
-
 class NetworkRailTest extends PHPUnit_Framework_TestCase{
 
 	public function testConstructor(){
 		$username = "JohnDoe";
 		$password = "Password123";
 		$securityToken = "7f8cac09-f446-e51f-a324-c528843e36f5";
-
-		$NR = new NetworkRail($username, $password, $securityToken);
-
-		$this->assertEquals($username, "JohnDoe");
-		$this->assertEquals($password, "Password123");
-		$this->assertEquals($securityToken, "7f8cac09-f446-e51f-a324-c528843e36f5");
+		print_r(get_declared_classes());
+		$NR = new conorhaining\phpopenraildata\NetworkRail($username, $password, $securityToken);
+		var_dump($NR);
+		$this->assertEquals($NR->getUsername(), "JohnDoe");
+		$this->assertEquals($NR->getPassword(), "Password123");
+		$this->assertEquals($NR->getSecurityToken(), "7f8cac09-f446-e51f-a324-c528843e36f5");
 	}
 }
