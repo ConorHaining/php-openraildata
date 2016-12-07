d1bd28819c38ad96a9e48140e2e8fda31ceb9d05
Conor Haining <conor.haining@gmail.com>
2016-12-07 17:21:44 +0000

    Added TOC codes

---
+++ src/NetworkRail/Codes/ATOCCode.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace conorhaining\phpopenraildata\NetworkRail\Codes;
+
+/**
+* A class which allows provides the ATOC codes for all the TOCs. The ATOC codes is used in the SCHEDULE feed.
+* 
+* @author Conor Haining <conor.haining@gmail.com>
+* @package conorhaining/php-openraildata
+* @since 0.0.1-dev
+* @see http://nrodwiki.rockshore.net/index.php/TOC_Codes
+*/
+class ATOCCode
+{
+	const ABEILLIO_GREATER_ANGLIA = "LE";
+	const ARRIVA_TRAINS_WALES = "AW";
+	const C2C = "CC";
+	const CALEDONIAN_SLEEPER = "CS";
+	const CHILTERN_RAILWAY = "CH";
+	const CROSSCOUNTRY = "XC";
+	const DEVON_AND_CORNWALL_RAILWAY = "DC";
+	const EAST_COAST = "GR";
+	const EUROSTAR = "ES";
+	const FIRST_CAPITAL_CONNECT = "FC";
+	const FIRST_GREAT_WESTERN = "GW";
+	const FIRST_HULL_TRAINS = "HT";
+	const FIRST_SCOTRAIL = "SR";
+	const FIRST_TRANSPENNINE_EXPRESS = "TP";
+	const GATWICK_EXPRESS = "GX";
+	const GB_RAILFREIGHT = "ZZ";
+	const GRAND_CENTRAL = "GC";
+	const GREAT_NORTH_WESTERN_RAILWAY = "LN";
+	const GOVIA_THAMESLINK_RAILWAY_GREAT_NORTHERN = "GN";
+    const GOVIA_THAMESLINK_RAILWAY_THAMESLINK = "TL";
+    const HEATHROW_CONNECT = "HC";
+    const HEATHROW_EXPRESS = "HX";
+    const ISLAND_LINES = "IL";
+    const LONDON_MIDLAND = "LM";
+    const LONDON_OVERGROUND = "LO";
+    const LUL_BAKERLOO_LINE = "LT";
+    const LUL_DISTRICT_LINE_WIMBLEDON = "LT";
+    const LUL_DISTRICT_LINE_RICHMOND = "LT";
+    const MERSEYRAIL = "ME";
+    const NEXUS_TYNE_AND_WEAR_METRO = "LR";
+    const NORTH_YORKSHIRE_MOORS_RAILWAY = "TW";
+    const NORTHERN_RAIL = "NT";
+    const SOUTH_WEST_TRAINS = "SW";
+    const SOUTHEASTERN = "SE";
+    const SOUTHERN = "SN";
+    const SOUTH_YORKSHIRE_SUPERTRAM = "SJ";
+    const TFL_RAIL = "XR";
+    const VIRGIN_TRAINS = "VT";
+    const WEST_COAST_RAILWAY_CO = "WR";
+	
+}
---
+++ src/NetworkRail/Codes/SectorCodes.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace conorhaining\phpopenraildata\NetworkRail\Codes;
+
+/**
+* A class which allows provides the Sector Codes used to represent the TOC in the TRUST message.
+* 
+* @author Conor Haining <conor.haining@gmail.com>
+* @package conorhaining/php-openraildata
+* @since 0.0.1-dev
+* @see http://nrodwiki.rockshore.net/index.php/TOC_Codes
+*/
+class SectorCode
+{
+	const ABEILLIO_GREATER_ANGLIA = 21;
+	const ARRIVA_TRAINS_WALES = 71;
+	const C2C = 79;
+	const CALEDONIAN_SLEEPER = 35;
+	const CHILTERN_RAILWAY = 74;
+	const CROSSCOUNTRY = 27;
+	const DEVON_AND_CORNWALL_RAILWAY = 34;
+	const EAST_COAST = 61;
+	const EUROSTAR = 06;
+	const FIRST_CAPITAL_CONNECT = 26;
+	const FIRST_GREAT_WESTERN = 25;
+	const FIRST_HULL_TRAINS = 55;
+	const FIRST_SCOTRAIL = 60;
+	const FIRST_TRANSPENNINE_EXPRESS = 20;
+	const GATWICK_EXPRESS = 81;
+	const GB_RAILFREIGHT = 54;
+	const GRAND_CENTRAL = 22;
+	const GREAT_NORTH_WESTERN_RAILWAY = NULL;
+	const GOVIA_THAMESLINK_RAILWAY_GREAT_NORTHERN = 88;
+    const GOVIA_THAMESLINK_RAILWAY_THAMESLINK = 88;
+    const HEATHROW_CONNECT = 24;
+    const HEATHROW_EXPRESS = 86;
+    const ISLAND_LINES = 85;
+    const LONDON_MIDLAND = 29;
+    const LONDON_OVERGROUND = 30;
+    const LUL_BAKERLOO_LINE = 91;
+    const LUL_DISTRICT_LINE_WIMBLEDON = 90;
+    const LUL_DISTRICT_LINE_RICHMOND = 93;
+    const MERSEYRAIL = 64;
+    const NEXUS_TYNE_AND_WEAR_METRO = 56;
+    const NORTH_YORKSHIRE_MOORS_RAILWAY = 51;
+    const NORTHERN_RAIL = 23;
+    const SOUTH_WEST_TRAINS = 84;
+    const SOUTHEASTERN = 80;
+    const SOUTHERN = 82;
+    const SOUTH_YORKSHIRE_SUPERTRAM = NULL;
+    const TFL_RAIL = 33;
+    const VIRGIN_TRAINS = 65;
+    const WEST_COAST_RAILWAY_CO = 50;
+	
+}
