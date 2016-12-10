ab20ceef852d6259f199918759d9bc822c189639
Conor Haining <conor.haining@gmail.com>
2016-12-10 00:36:43 +0000

    Thinned tester

--- tests/NetworkRailTest.php
+++ tests/NetworkRailTest.php
@@ -1,14 +1,16 @@
 <?php
 
+use conorhaining\phpopenraildata\NetworkRail;
+
 class NetworkRailTest extends PHPUnit_Framework_TestCase{
 
 	public function testConstructor(){
 		$username = "JohnDoe";
 		$password = "Password123";
 		$securityToken = "7f8cac09-f446-e51f-a324-c528843e36f5";
-		print_r(get_declared_classes());
-		$NR = new conorhaining\phpopenraildata\NetworkRail($username, $password, $securityToken);
-		var_dump($NR);
+
+		$NR = new NetworkRail($username, $password, $securityToken);
+		
 		$this->assertEquals($NR->getUsername(), "JohnDoe");
 		$this->assertEquals($NR->getPassword(), "Password123");
 		$this->assertEquals($NR->getSecurityToken(), "7f8cac09-f446-e51f-a324-c528843e36f5");
